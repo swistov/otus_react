40d32fffc5536304c839ae03dab57ac3f722d195
Svistov Nikolai <swistov@gmail.com>
2019-10-07 22:46:49 +0300

    Lesson => 36

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input className='search-input'
+               placeholder={'type to search'} />);
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import SearchPanel from "./components/search-panel";
-import MenuPanel from "./components/menu-panel";
-import FilterLeft from "./components/filter-left";
-import ListItem from "./components/list-item";
-
-const App = () => {
-
-    const todoData = [
-        { label: 'Drink coffe', important: false },
-        { label: 'Make awesome app', important: true },
-        { label: 'Have a lunch', important: false },
-    ]
-
-    return (
-    <div>
-        <MenuPanel/>
-        <FilterLeft/>
-        <SearchPanel/>
-        <li>
-            <ListItem label={'Some text'}/>
-        </li>
-        <li>
-            <ListItem label={'Build my App'}
-                      important />
-        </li>
-    </div>
-    );
-};
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
